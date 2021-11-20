f29878194f0d58a1b299b3d04c904b4cab144f39
uoy-jb2501 <94061765+uoy-jb2501@users.noreply.github.com>
2021-11-20 18:10:41 +0000

    Set up libGDX project

---
+++ desktop/src/tk/shardsoftware/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tk.shardsoftware.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tk.shardsoftware.PirateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PirateGame(), config);
+	}
+}
