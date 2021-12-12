1b3685cf8836530afd4f65aa15de9fc3433f02fe
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2021-12-12 11:50:02 +0000

    Set default resolution to 720p

--- desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
+++ desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ import tk.shardsoftware.PirateGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new PirateGame(), config);
 	}
 }
