2811ad7c3b70ff8fe4ee84c65a1d7965fb335667
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-01-14 17:50:43 +0000

    Added basic player controls and debug screen

--- core/src/main/java/tk/shardsoftware/screens/GameScreen.java
+++ core/src/main/java/tk/shardsoftware/screens/GameScreen.java
@@ -1,35 +1,42 @@
 package tk.shardsoftware.screens;
 
+import static tk.shardsoftware.PirateGame.DEBUG_MODE;
+import static tk.shardsoftware.util.ResourceUtil.font;
 import static tk.shardsoftware.util.ResourceUtil.getTileTexture;
+
+import java.util.List;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 import tk.shardsoftware.World;
 import tk.shardsoftware.WorldMap;
+import tk.shardsoftware.entity.EntityShip;
 
 /** Handles game controls, rendering, and logic */
 public class GameScreen implements Screen {
 
-	private SpriteBatch batch;
+	private SpriteBatch batch, hudBatch;
 	private OrthographicCamera camera;
 	private WorldMap worldMap; // TODO: Integrate WorldMap into World
 
 	private World worldObj;
 
-	private int cameraSize = 100; // width and length of the camera's viewport.
+	// width and length of the camera's viewport.
+	private int cameraSize = (int) (720 / 2);
 
 	// /** The ship object that the player will control*/
-	// private EntityShip player;
+	private EntityShip player;
 
 	public GameScreen(AssetManager assets) {
 		batch = new SpriteBatch();
+		hudBatch = new SpriteBatch();
 		camera = new OrthographicCamera(cameraSize * 16f / 9f, cameraSize);
 		worldObj = new World();
 
@@ -42,9 +49,9 @@ public class GameScreen implements Screen {
 		worldMap = new WorldMap(1, 100, 100, worldTextures);
 		worldMap.buildWorld();
 
-		// player = new EntityShip(worldObj);
+		player = new EntityShip(worldObj);
 
-		// worldObj.getEntities().add(player);
+		worldObj.getEntities().add(player);
 	}
 
 	@Override
@@ -52,37 +59,89 @@ public class GameScreen implements Screen {
 
 	}
 
-	public void exampleCamera() { // example camera that moves with WASD.
+	public void controls() { // TODO: Rotate ship with controls
 		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-			camera.translate(0, 1);
+			// player.addVelocity(0, 1);
+			rotatePlayer(90);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-			camera.translate(0, -1);
+			// player.addVelocity(0, -1);
+			rotatePlayer(270);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-			camera.translate(-1, 0);
+			// player.addVelocity(-1, 0);
+			rotatePlayer(180);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-			camera.translate(1, 0);
+			// player.addVelocity(1, 0);
+			rotatePlayer(0);
+		}
+		if (DEBUG_MODE && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			player.getVelocity().setZero();
+		}
+		// player.setPosition(0, 0);
+		// System.out.println(player.getVelocity());
+		// System.out.println(player.getDirection());
+	}
+
+	private float goalAngle;
+
+	// TODO: Prevent multi-key speedup in turning bug
+	private void rotatePlayer(float goalAngle) {
+		float angle = player.getDirection();
+		float speed = player.getVelocity().len();
+
+		double rads = Math.toRadians(angle);
+		player.addVelocity((float) Math.cos(rads), (float) Math.sin(rads));
+
+		if (angle <= 90 && goalAngle >= 270) goalAngle -= 360;
+		if (angle >= 270 && goalAngle <= 90) goalAngle += 360;
+		if (angle > 180 && goalAngle < 90) goalAngle += 360;
+		this.goalAngle = goalAngle;
+
+		if (angle > goalAngle) {
+			player.rotate(-2);
+		} else if (angle < goalAngle) {
+			player.rotate(2);
 		}
 	}
 
 	@Override
 	public void render(float delta) {
-		// Set ProjectionMatrix of SpriteBatch (2)
+		controls();
 		logic();
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clears the buffer
+		ScreenUtils.clear(0, 0, 0, 1); // clears the buffer
 		batch.setProjectionMatrix(camera.combined);
-		exampleCamera();
 		camera.update();
 		batch.begin();
 		worldMap.drawTilesInRange(camera, cameraSize, batch);
 		renderEntities();
 		batch.end();
+
+		if (DEBUG_MODE) {
+			hudBatch.begin();
+			renderDebug(generateDebugStrings());
+			hudBatch.end();
+		}
+
+	}
+
+	private List<String> generateDebugStrings() {
+		return List.of("Current angle: " + player.getDirection(),
+				"Goal angle: " + goalAngle);
+	}
+
+	public void renderDebug(List<String> debugList) {
+		for (int i = 0; i < debugList.size(); i++) {
+			font.draw(hudBatch, debugList.get(i), 0,
+					Gdx.graphics.getHeight() - (font.getLineHeight()) * i);
+		}
 	}
 
 	private void logic() {
 		worldObj.update(Gdx.graphics.getDeltaTime());
+		player.getVelocity().scl(0.99f); // TODO: Improve water drag
+		// player.setPosition(Vector2.Zero); // good for debugging
 	}
 
 	public void renderEntities() {
@@ -94,7 +153,7 @@ public class GameScreen implements Screen {
 			batch.draw(e.getTexture(), e.getPosition().x, e.getPosition().y,
 					e.getHitbox().width / 2, e.getHitbox().height / 2,
 					e.getHitbox().width, e.getHitbox().height, 1, 1,
-					e.getDirection() - 90, false);
+					e.getDirection(), false);
 		});
 	}
 
