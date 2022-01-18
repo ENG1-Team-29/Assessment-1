3fd03a81044ae3c5a030267dfb42292d11ff422c
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-01-18 22:32:15 +0000

    Prevented resizing
    This is to prevent users from changing the aspect ratio from the
    predefined ones (yet to be implemented).

--- desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
+++ desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
@@ -6,9 +6,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import tk.shardsoftware.PirateGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setWindowedMode(1280, 720);
+		config.setResizable(false);
 		new Lwjgl3Application(new PirateGame(), config);
 	}
 }
