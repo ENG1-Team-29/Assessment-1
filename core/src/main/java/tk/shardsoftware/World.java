f354719b6324aa24997244e73800ae33768fcbbe
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-01-23 17:28:09 +0000

    Added cannonball and damageable list
    This is to allow easy calculation of cannonball intersection with
    damageable entities.

--- core/src/main/java/tk/shardsoftware/World.java
+++ core/src/main/java/tk/shardsoftware/World.java
@@ -2,11 +2,14 @@ package tk.shardsoftware;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.badlogic.gdx.math.MathUtils;
 
 import tk.shardsoftware.entity.Entity;
+import tk.shardsoftware.entity.EntityCannonball;
+import tk.shardsoftware.entity.IDamageable;
 
 /** @author James Burnell */
 public class World {
@@ -18,14 +21,23 @@ public class World {
 	public static final int WORLD_HEIGHT = 300;
 	public static final int WORLD_TILE_SIZE = 10;
 
-	/** The collection of entities that are in the world */
+	/** The collection of entities that are in the world. */
 	private List<Entity> entities;
+	/** The collection of cannonballs within the world. */
+	private List<EntityCannonball> cannonballs;
+	/**
+	 * The collection of damageable objects that are in the world. This includes
+	 * entities and non-entities such as college buildings.
+	 */
+	private List<IDamageable> damagableObjs;
 
 	/** The map of the world */
 	public WorldMap worldMap;
 
 	public World() {
 		entities = new ArrayList<Entity>();
+		damagableObjs = new ArrayList<IDamageable>();
+		cannonballs = new ArrayList<EntityCannonball>();
 
 		this.worldMap = new WorldMap(WORLD_TILE_SIZE, WORLD_WIDTH, WORLD_HEIGHT);
 		if (BUILD_MAP) {
@@ -43,6 +55,26 @@ public class World {
 	 */
 	public void update(float delta) {
 		updateEntities(delta);
+		updateCannonballs();
+	}
+
+	/**
+	 * Find intersections between cannonballs and damageable objects. If such an
+	 * intersection is found, it will call
+	 * {@link EntityCannonball#onTouchingDamageable(IDamageable)}.
+	 */
+	private void updateCannonballs() {
+		for (EntityCannonball c : cannonballs) {
+			for (IDamageable dmgObj : damagableObjs) {
+				// Skip object if it is the parent of the cannonball
+				if (c.isObjParent(dmgObj)) continue;
+				// Test for intersection
+				if (c.getHitbox().overlaps(dmgObj.getHitbox())) {
+					c.onTouchingDamageable(dmgObj);
+					break;
+				}
+			}
+		}
 	}
 
 	/**
@@ -51,11 +83,19 @@ public class World {
 	 */
 	private void updateEntities(float delta) {
 		Iterator<Entity> iter = entities.iterator();
+		LinkedList<Entity> removeList = new LinkedList<Entity>();
 		while (iter.hasNext()) {
 			Entity e = iter.next();
 			e.update(delta);
-			if (e.remove) iter.remove();
+			if (e.remove) removeList.add(e);
 		}
+
+		removeList.forEach(e -> {
+			entities.remove(e);
+			if (e instanceof IDamageable) damagableObjs.remove((IDamageable) e);
+			if (e instanceof EntityCannonball) cannonballs.remove((EntityCannonball) e);
+			e.onRemove();
+		});
 	}
 
 	/** The list of entities contained within the world */
@@ -65,6 +105,12 @@ public class World {
 
 	public void addEntity(Entity e) {
 		entities.add(e);
+		if (e instanceof IDamageable) {
+			damagableObjs.add((IDamageable) e);
+		}
+		if (e instanceof EntityCannonball) {
+			cannonballs.add((EntityCannonball) e);
+		}
 	}
 
 	public static float getWidth() {
