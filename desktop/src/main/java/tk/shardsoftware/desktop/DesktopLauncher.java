c2ca72f9d555f9424e6a14582bde49e41826c070
James Burnell <94061765+uoy-jb2501@users.noreply.github.com>
2022-02-01 20:42:27 +0000

    Added disabled line for fullscreen

--- desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
+++ desktop/src/main/java/tk/shardsoftware/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setWindowedMode(1280, 720);
+		// config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setResizable(false);
 		config.setForegroundFPS(60);
 		config.useVsync(false);
